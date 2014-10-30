4534156fc9337248e313eed9568baaa87e61b3eb
Kyle L. Jensen <kljensen@gmail.com>
2014-10-30 15:03:04 -0400

    Use PORT from environment for server

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
 
 // Start the server on port 3000
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
