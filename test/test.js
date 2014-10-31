5fea13f0367701422277a093c055043a1dc62563
Kyle L. Jensen <kljensen@gmail.com>
2014-10-31 16:32:01 -0400

    Add test for /about link in footer

--- test/test.js
+++ test/test.js
@@ -3,7 +3,10 @@
 var assert = require('assert');
 var app = require('../app.js');
 var Browser = require('zombie');
-
+var async = require('async');
+var chai = require('chai');
+var _ = require('lodash');
+var expect = chai.expect;
 
 describe('The home page',function(){
   before(function(done){
@@ -20,15 +23,30 @@ describe('The home page',function(){
   });
 
   it('should be up and running', function(){
-    assert.ok(this.browser.statusCode === 200);
+    expect(this.browser.success).to.be.ok;
   });
 
   it('should include bootstrap css', function(done){
-    var dis = this;
-    this.browser.visit(dis.site, function(){
-      done();      
+    expect(this.browser.query('link[href*="bootstrap.css"]')).to.be.ok;
+  });
+
+  it('should have a link to the about page in the footer of all pages that exist', function(done){
+    var browser = this.browser;
+    function getAboutLink (url, callback) {
+      browser.visit(url, function(){
+        if (!browser.success) {
+          return callback(new Error("Page does not exist"), false);
+        };
+        return callback(null, expect(browser.query('footer a[href="/about"]')).to.be.ok); 
+      });
+    }
+    var urls = ['/', '/about', '/events/new', '/events/0', '/events/1', '/events/2'];
+    async.mapSeries(urls, getAboutLink, function(err, results){
+      expect(_.all(results)).to.be.ok;
+      done();
     });
   });
+
   it('should refuse empty submissions');
   it('should refuse partial submissions');
   it('should keep values on partial submissions');
