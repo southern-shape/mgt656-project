49f3ab1610d164f11b299abadb9732510b2eff77
Kyle L. Jensen <kljensen@gmail.com>
2014-10-31 10:00:54 -0400

    Add gulp and jshint

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+// Gulpfile.js - We use Gulp to monitor our files and
+// do two things when we make changes to the code:
+// 1) check it for errors ("lint it") and 2) restart
+// the application.
+'use strict';
+
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function () {
+  gulp.src(['./**/*.js', '!./node_modules/**'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('develop', function () {
+  nodemon({ script: 'start-app.js', ext: 'html js'})
+    .on('change', ['lint'])
+    .on('restart', function () {
+      console.log('restarted!');
+    });
+});
+
