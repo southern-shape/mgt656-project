f66893b5047df5c6951edbe6ec0520c963fcadbc
Kyle L. Jensen <kljensen@gmail.com>
2014-10-31 09:43:56 -0400

    Get Zombie working for tests

--- test/test.js
+++ test/test.js
@@ -2,22 +2,38 @@ var assert = require('assert');
 var app = require('../app.js');
 var Browser = require('zombie');
 
-describe('Web site',function(){
 
-  before(function(){
-    var port = process.env.PORT | 3005;
-    this.server = app.listen(port);
-    this.browser = new Browser({site: 'http://localhost:' + port});
+describe('The home page',function(){
+  before(function(done){
+    this.port = process.env.PORT || 3005;
+    this.site = 'http://localhost:' + this.port;
+    this.browser = new Browser({
+      site: this.site,
+    });
+    this.server = app.listen(this.port, done);
+  });
+
+  before(function(done){
+    this.browser.visit(this.site, done);
+  });
+
+  it('should be up and running', function(){
+    assert.ok(this.browser.statusCode === 200);
   });
 
   it('should include bootstrap css', function(done){
-    this.browser.visit('/', done);
-    console.log(this.browser.success);
-    assert.ok(this.browser.success);
+    var dis = this;
+    this.browser.visit(dis.site, function(){
+      done();      
+    });
   });
   it('should refuse empty submissions');
   it('should refuse partial submissions');
   it('should keep values on partial submissions');
   it('should refuse invalid emails');
   it('should accept complete submissions');
+
+  after(function(done){
+    this.server.close(done);
+  });
 });
