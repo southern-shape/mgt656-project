df059784b2cc26c84b4ff06b517cd16d5b3100c6
Kyle L. Jensen <kljensen@gmail.com>
2014-11-03 12:58:38 -0500

    Add real datetimes to events

--- models/events.js
+++ models/events.js
@@ -7,21 +7,24 @@ var allEvents = [
   {
     id: 0,
     title: 'SOM House Party',
-    date: 'This Friday',
+    // Note that JavaScript months are zero-indexed,
+    // so, month zero is January. This is Jan 17th
+    // 2013 at 4:30pm local time.
+    date:   new Date(2016, 0, 17, 16, 30, 0),
     image: 'http://i.imgur.com/pXjrQ.gif',
     attending: ['kyle.jensen@yale.edu', 'kim.kardashian@yale.edu']
   },
   {
     id: 1,
     title: 'BBQ party for hackers and nerds',
-    date: '6/1/2015',
+    date:   new Date(2015, 8, 1, 19, 0, 0),
     image: null,
     attending: ['kyle.jensen@yale.edu', 'kim.kardashian@yale.edu']
   },
   {
     id: 2,
     title: 'BBQ for managers',
-    date: '7/1/2015',
+    date:   new Date(2015, 9, 20, 18, 0, 0),
     image: 'http://i.imgur.com/0qylcLh.jpg',
     attending: ['kim.kardashian@yale.edu'],
   }
