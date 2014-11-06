ddc94d215cad00150affd85912acf3afa00000d5
Kyle L. Jensen <kljensen@gmail.com>
2014-11-05 20:48:56 -0500

    Add some date data students will find useful completing the project

--- controllers/events.js
+++ controllers/events.js
@@ -3,6 +3,32 @@
 var events = require('../models/events');
 var validator = require('validator');
 
+// Date data that would be useful to you
+// completing the project These data are not
+// used a first.
+//
+var allowedDateInfo = {
+  months: {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
+  },
+  minutes: [0, 30],
+  hours: [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+    12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+  ]
+}
+
 /**
  * Controller that renders a list of events in HTML.
  */
