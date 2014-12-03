16a81442d4c6b9e13b424b2e29a9b4a1f28f1915
Kyle L. Jensen <kljensen+cloud9@gmail.com>
2014-12-03 22:00:24 +0000

    Fix PORT environment variable

--- start-app.js
+++ start-app.js
@@ -3,7 +3,7 @@
 var app = require('./app.js');
 
 // Start the server on port 3000
-var port = process.env.port || 3500;
+var port = process.env.PORT || 3500;
 var ip = process.env.IP || 'localhost';
 var server = app.listen(port, ip, function() {
   console.log('Your app is running at http://' + process.env.C9_HOSTNAME || process.env.IP + ':' + process.env.PORT);
