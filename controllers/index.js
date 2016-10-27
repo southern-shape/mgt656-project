ef90348bd5854f84b1200f526e2c76da5f8c4e57
Ethan <ethan.joseph@yale.edu>
2016-10-27 13:28:03 +0000

    Add events list to homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,16 @@
 'use strict';
 
+var events = require('../models/events');
+
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
   var contextData = {
     'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).'
+    'tagline': 'You are doomed (just kidding).',
+    'events': events.all
+    
   };
   response.render('index.html', contextData);
 }
