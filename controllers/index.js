c4bc628f7ced9f4f1c7f121ed658d6e6030417b3
Kenny McCormick <vassilis.boukas@yale.edu>
2016-11-01 01:37:17 +0000

    Hide old events

--- controllers/index.js
+++ controllers/index.js
@@ -6,12 +6,18 @@ var events = require('../models/events');
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var now = new Date();
   var contextData = {
     'title': 'MGT 656',
     'tagline': 'You are doomed (just kidding).',
-    'events': events.all
-    
+    'events': []
   };
+  for (var i=0; i < events.all.length; i++) {
+    var event = events.all[i];
+    if (event.date > now) {
+      contextData.events.push(event);
+    }
+  }
   response.render('index.html', contextData);
 }
 
--- models/events.js
+++ models/events.js
@@ -10,7 +10,7 @@ var allEvents = [
     // Note that JavaScript months are zero-indexed,
     // so, month zero is January. This is Jan 17th
     // 2013 at 4:30pm local time.
-    date:   new Date(2016, 0, 17, 16, 30, 0),
+    date:   new Date(2016, 12, 17, 16, 30, 0),
     image: 'http://i.imgur.com/pXjrQ.gif',
     location: 'Kyle \'s house',
     attending: ['kyle.jensen@yale.edu', 'kim.kardashian@yale.edu']
@@ -32,7 +32,7 @@ var allEvents = [
     attending: ['kim.kardashian@yale.edu'],
   },
   {
-    id: 4,
+    id: 3,
     title: 'Cooking lessons for the busy business student',
     date:   new Date(2014, 8, 2, 19, 0, 0),
     image: 'http://i.imgur.com/02KT9.gif',
